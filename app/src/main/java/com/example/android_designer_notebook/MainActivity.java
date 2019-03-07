290b6038d9ed5e129eeca54412be40be60f2280c
Minikhil <31946890+Minikhil@users.noreply.github.com>
2019-03-07 12:57:50 -0500

    Getting started completed

---
+++ app/src/main/java/com/example/android_designer_notebook/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android_designer_notebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
